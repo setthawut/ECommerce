38556f7ba8f7c428256e23ffe36bb72a38eba2eb
Aun <aunsetthawut@gmail.com>
2020-12-05 23:17:57 +0700

    update

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,17 +1,66 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Rating from "../components/Rating";
 import data from "../data";
 
 const ProductScreen = (props) => {
-  const product = data.products.find(
-    (x) => x._id === this.props.match.params.id,
-  );
+  const product = data.products.find((x) => x._id === props.match.params.id);
+  if (!product) {
+    return <div>Product Not Found</div>;
+  }
   return (
-    <div className="row">
-      <div className="col-2">
-        <img src={product.imge} alt={product.name}></img>
+    <div>
+      <Link to="/">Back to result</Link>
+      <div className="row top">
+        <div className="col-2">
+          <img src={product.image} alt={product.name}></img>
+        </div>
+        <div className="col-1">
+          <ul>
+            <li>
+              <h1> {product.name}</h1>
+            </li>
+            <li>
+              <Rating
+                rating={product.rating}
+                numReviw={product.numReviews}
+              ></Rating>
+            </li>
+            <li>Price : ${product.price}</li>
+            <li>
+              Description:<p>{product.description}</p>
+            </li>
+          </ul>
+        </div>
+        <div className="col-1">
+          <div className="card card-body">
+            <ul>
+              <li>
+                <div className="row">
+                  <div>Price</div>
+                  <div className="price">${product.price}</div>
+                </div>
+              </li>
+              <li>
+                <div className="row">
+                  <div>Status</div>
+                  <div className="price">
+                    $
+                    {product.countInStock > 0 ? (
+                      <span className="success">In Stock</span>
+                    ) : (
+                      <span className="error">Unavailable</span>
+                    )}
+                  </div>
+                </div>
+              </li>
+              <li>
+                <button className="primary block"> Add to Cart</button>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
-      <div className="col-1"></div>
-      <div className="col-1"></div>
     </div>
   );
 };
