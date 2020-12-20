50dacc2634c5348561f2ff655f20aeb47533ae16
Aun <aunsetthawut@gmail.com>
2020-12-20 11:30:45 +0700

    update backend

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -4,6 +4,7 @@ export const cartReducer = (state = { cartItems: [] }, action) => {
   switch (action.type) {
     case CART_ADD_ITEM:
       const item = action.payload;
+      console.log(item)
       const existItem = state.cartItems.find((x) => x.product === item.product);
       if (existItem) {
         return {
