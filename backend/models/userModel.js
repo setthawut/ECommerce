0c5439bf8af19bfd06ce99513dab0cc020675fc2
Aun <aunsetthawut@gmail.com>
2020-12-24 20:06:00 +0700

    update

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,4 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
   {
@@ -9,7 +9,7 @@ const userSchema = new mongoose.Schema(
   },
   {
     timestamps: true,
-  }
+  },
 );
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 export default User;
