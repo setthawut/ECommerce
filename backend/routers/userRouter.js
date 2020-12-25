1b4783e46ff97f13481b192b3b02ddcc935a2462
Aun <aunsetthawut@gmail.com>
2020-12-25 15:30:47 +0700

    update create get by mongo

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -1,17 +1,18 @@
-import express from 'express';
-import expressAsyncHandler from 'express-async-handler';
-import data from '../data.js';
-import User from '../models/userModel.js';
+import express from "express";
+import expressAsyncHandler from "express-async-handler";
+import data from "../data.js";
+import Product from "../models/productModel.js";
+import User from "../models/userModel.js";
 
 const userRouter = express.Router();
 
 userRouter.get(
-  '/seed',
+  "/seed",
   expressAsyncHandler(async (req, res) => {
-     await User.remove({});
+    await User.remove({});
     const createdUsers = await User.insertMany(data.users);
     res.send({ createdUsers });
-  })
+  }),
 );
 
 export default userRouter;
