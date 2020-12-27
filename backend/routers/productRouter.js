f261a9548f00b034fc73bc6809339df9ad639a72
Aun <aunsetthawut@gmail.com>
2020-12-27 11:41:47 +0700

    update

--- backend/routers/productRouter.js
+++ backend/routers/productRouter.js
@@ -33,4 +33,6 @@ productRouter.get(
   }),
 );
 
+
+
 export default productRouter;
--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -1,8 +1,9 @@
 import express from "express";
 import expressAsyncHandler from "express-async-handler";
 import data from "../data.js";
-import Product from "../models/productModel.js";
 import User from "../models/userModel.js";
+import bcrypt from "bcryptjs";
+import { gennerateToken } from "../utils.js";
 
 const userRouter = express.Router();
 
@@ -12,6 +13,27 @@ userRouter.get(
     await User.remove({});
     const createdUsers = await User.insertMany(data.users);
     res.send({ createdUsers });
+  }),
+);
+
+userRouter.post(
+  "/signin",
+  expressAsyncHandler(async (req, res) => {
+    const user = await User.findOne({ email: req.body.email });
+
+    if (user) {
+      if (bcrypt.compareSync(req.body.password, user.password)) {
+        res.send({
+          _id: user._id,
+          name: user.name,
+          email: user.email,
+          isAdmin: user.isAdmin,
+          token: gennerateToken(user),
+        });
+        return;
+      }
+    }
+    res.status(401).send({ message: "Invalid email or password" });
   }),
 );
 
